822603e1ce9cff3844c791c0e2e6da460369b081
Rommel Rico <me@rommelrico.com>
2018-06-15 08:42:54 -0700

    Loading the Scene using the FXMLoader

--- src/com/rommelrico/Main.java
+++ src/com/rommelrico/Main.java
@@ -1,14 +1,8 @@
 package com.rommelrico;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -16,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Pane pane = new FlowPane();
+        Pane pane = FXMLLoader.load(getClass().getResource("MainLayout.fxml"));
 
         Scene scene = new Scene(pane);
         primaryStage.setTitle("Hello World");
