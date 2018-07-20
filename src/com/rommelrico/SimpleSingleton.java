241382e467331ca04fcd9d505c03cbcf311b1a54
Rommel Rico <me@rommelrico.com>
2018-07-20 09:16:17 -0700

    SimpleSingleton

---
+++ src/com/rommelrico/SimpleSingleton.java
@@ -0,0 +1,11 @@
+package com.rommelrico;
+
+public class SimpleSingleton {
+    private static SimpleSingleton ourInstance = new SimpleSingleton();
+
+    public static SimpleSingleton getInstance() {
+        return ourInstance;
+    }
+
+    private SimpleSingleton() { }
+}
