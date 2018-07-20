2ba62f5dafa0c87dd19c38d3ed2768cb1c8502c6
Rommel Rico <me@rommelrico.com>
2018-07-20 09:03:43 -0700

    Adding a pane to the new detail window

--- src/com/rommelrico/MainController.java
+++ src/com/rommelrico/MainController.java
@@ -4,15 +4,19 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.Comparator;
 import java.util.ResourceBundle;
@@ -99,7 +103,17 @@ public class MainController implements Initializable {
         });
 
         showDetails.setOnAction(e -> {
+            Pane pane = null;
+            try {
+                pane = FXMLLoader.load(getClass().getResource("MainLayout.fxml"));
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+            Scene scene = new Scene(pane);
+            scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+
             Stage stage = new Stage();
+            stage.setScene(scene);
             stage.show();
         });
 
