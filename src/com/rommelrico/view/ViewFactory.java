79ca4d5991200c3e4c2c66a8cbad48af91cb5a07
Rommel Rico <me@rommelrico.com>
2018-07-25 08:12:30 -0700

    ViewFactory class and getMainScene()

---
+++ src/com/rommelrico/view/ViewFactory.java
@@ -0,0 +1,24 @@
+package com.rommelrico.view;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+
+import java.io.IOException;
+
+public class ViewFactory {
+
+    public Scene getMainScene() {
+        Pane pane;
+
+        try {
+            pane = FXMLLoader.load(getClass().getResource("MainLayout.fxml"));
+        } catch (IOException e) {
+            pane = new Pane();
+        }
+
+        Scene scene = new Scene(pane);
+        scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+        return scene;
+    }
+}
