f7f02d3e05cf312f6d8b0ed5e65476d97acbf29e
Rommel Rico <me@rommelrico.com>
2018-07-26 08:38:56 -0700

    Removing unused imports

--- src/com/rommelrico/view/ViewFactory.java
+++ src/com/rommelrico/view/ViewFactory.java
@@ -10,10 +10,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Pane;
-
 import javax.naming.OperationNotSupportedException;
-import java.io.IOException;
 
 public class ViewFactory {
 
