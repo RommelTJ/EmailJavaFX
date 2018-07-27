f3edeb2ed087e51750ade2c714af5bde33840a7d
Rommel Rico <me@rommelrico.com>
2018-07-27 09:41:57 -0700

    Making the View updateable

--- src/com/rommelrico/model/table/BoldableRowFactory.java
+++ src/com/rommelrico/model/table/BoldableRowFactory.java
@@ -1,8 +1,29 @@
 package com.rommelrico.model.table;
 
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.control.TableRow;
 
 public class BoldableRowFactory<T extends AbstractTableItem> extends TableRow<T> {
+
+    private final SimpleBooleanProperty bold = new SimpleBooleanProperty();
+    private T currentItem = null;
+
+    public BoldableRowFactory() {
+        super();
+        bold.addListener((ObservableValue<? extends  Boolean> observableValue, Boolean oldValue, Boolean newValue) -> {
+            if (currentItem != null && currentItem == getItem()) {
+                updateItem(getItem(), isEmpty());
+            }
+        });
+        itemProperty().addListener((ObservableValue<? extends  T> observableValue, T oldValue, T newValue) -> {
+            bold.unbind();
+            if (newValue != null) {
+                bold.bind(newValue.readProperty());
+                currentItem = newValue;
+            }
+        });
+    }
 
     @Override
     final protected void updateItem(T item, boolean empty) {
