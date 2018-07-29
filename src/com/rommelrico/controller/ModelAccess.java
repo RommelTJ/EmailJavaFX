ee3a5729454e0eb9a2d84b8ad365c59799d1c58f
Rommel Rico <rommeltj@gmail.com>
2018-07-29 05:54:23 -0700

    Adding the EmailFolderBean to the ModelAccess class

--- src/com/rommelrico/controller/ModelAccess.java
+++ src/com/rommelrico/controller/ModelAccess.java
@@ -1,10 +1,12 @@
 package com.rommelrico.controller;
 
 import com.rommelrico.model.EmailMessageBean;
+import com.rommelrico.model.folder.EmailFolderBean;
 
 public class ModelAccess {
 
     private EmailMessageBean selectedMessage;
+    private EmailFolderBean<String> selectedFolder;
 
     public EmailMessageBean getSelectedMessage() {
         return selectedMessage;
@@ -12,6 +14,14 @@ public class ModelAccess {
 
     public void setSelectedMessage(EmailMessageBean selectedMessage) {
         this.selectedMessage = selectedMessage;
+    }
+
+    public EmailFolderBean<String> getSelectedFolder() {
+        return selectedFolder;
+    }
+
+    public void setSelectedFolder(EmailFolderBean<String> selectedFolder) {
+        this.selectedFolder = selectedFolder;
     }
 
 }
