bb2bd53d10a1880cf9e314b70ae45e579aec1804
Rommel Rico <me@rommelrico.com>
2018-07-30 08:18:49 -0700

    Testing

---
+++ src/com/rommelrico/Test.java
@@ -0,0 +1,17 @@
+package com.rommelrico;
+
+import com.rommelrico.model.EmailAccountBean;
+import com.rommelrico.model.EmailMessageBean;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class Test {
+
+    public static void main(String[] args) {
+        EmailAccountBean emailAccountBean = new EmailAccountBean("me@rommelrico.com", "REDACTED");
+
+        ObservableList<EmailMessageBean> data = FXCollections.observableArrayList();
+        emailAccountBean.addEmailsToData(data);
+    }
+
+}
