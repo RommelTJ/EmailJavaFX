08a29fd4044c68b71db50f16644260ebd95b3974
Rommel Rico <me@rommelrico.com>
2018-08-05 14:29:46 -0700

    Fix for MessageRendererService

--- src/com/rommelrico/controller/services/MessageRendererService.java
+++ src/com/rommelrico/controller/services/MessageRendererService.java
@@ -13,7 +13,7 @@ import javax.mail.Multipart;
 import javax.mail.internet.MimeBodyPart;
 import java.io.IOException;
 
-public class MessageRendererService extends Service<Void> {
+public class MessageRendererService extends Service<Void> implements Runnable {
 
     private EmailMessageBean messageToRender;
     private WebEngine messageRendererEngine;
@@ -47,25 +47,31 @@ public class MessageRendererService extends Service<Void> {
                     || messageType.contains("TEXT/PLAIN")
                     || messageType.contains("text")) {
                 sb.append(message.getContent().toString());
+
             } else if (messageType.contains("multipart")) {
                 Multipart mp = (Multipart) message.getContent();
-                for (int i = mp.getCount() -1; i >= 0; i--) {
+                for (int i = mp.getCount() - 1; i >= 0; i--) {
                     BodyPart bp = mp.getBodyPart(i);
                     String contentType = bp.getContentType();
                     if (contentType.contains("TEXT/HTML")
-                            || messageType.contains("TEXT/PLAIN")
-                            || messageType.contains("mixed")
-                            || messageType.contains("text")) {
-                        if (sb.length() == 0) {
+                            || contentType.contains("TEXT/PLAIN")
+                            || contentType.contains("mixed")
+                            || contentType.contains("text")) {
+                        // Here the risk of incomplete messages are shown, for messages that contain both
+                        // html and text content, but these messages are very rare;
+                        if (sb.length()== 0) {
                             sb.append(bp.getContent().toString());
                         }
                     } else if (contentType.toLowerCase().contains("application")) {
-                        MimeBodyPart mbp = (MimeBodyPart) bp;
+                        MimeBodyPart mbp = (MimeBodyPart)bp;
+                        // Sometimes the text content of the message is encapsulated in another multipart,
+                        // so we have to iterate again through it.
                     } else if (bp.getContentType().contains("multipart")) {
-                        Multipart mp2 = (Multipart) bp.getContent();
-                        for (int j = mp2.getCount() - 1; j >= 0; j--) {
+                        Multipart mp2 = (Multipart)bp.getContent();
+                        for (int j = mp2.getCount()-1; j >= 0; j--) {
                             BodyPart bp2 = mp2.getBodyPart(i);
-                            if (bp2.getContentType().contains("TEXT/HTML") || bp2.getContentType().contains("TEXT/PLAIN")) {
+                            if ((bp2.getContentType().contains("TEXT/HTML")
+                                    || bp2.getContentType().contains("TEXT/PLAIN"))) {
                                 sb.append(bp2.getContent().toString());
                             }
                         }
@@ -78,4 +84,8 @@ public class MessageRendererService extends Service<Void> {
         }
     }
 
+    @Override
+    public void run() {
+        renderMessage();
+    }
 }
