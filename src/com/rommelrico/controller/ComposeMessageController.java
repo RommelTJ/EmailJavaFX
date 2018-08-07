9962917746ca45f9c48e8ebdbcef6d39b8285e78
Rommel Rico <me@rommelrico.com>
2018-08-07 08:25:18 -0700

    Initial work on the ComposeMessageController

---
+++ src/com/rommelrico/controller/ComposeMessageController.java
@@ -0,0 +1,48 @@
+package com.rommelrico.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ComposeMessageController extends AbstractController implements Initializable {
+
+    @FXML
+    private Label attachmentsLabel;
+
+    @FXML
+    private ChoiceBox<?> senderChoice;
+
+    @FXML
+    private TextField recepientField;
+
+    @FXML
+    private TextField subjectField;
+
+    @FXML
+    private Label errorLabel;
+
+    @FXML
+    void attchBtnAction() {
+
+    }
+
+    @FXML
+    void sendBtnAction() {
+
+    }
+
+    public ComposeMessageController(ModelAccess modelAccess) {
+        super(modelAccess);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
